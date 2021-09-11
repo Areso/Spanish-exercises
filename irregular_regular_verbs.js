b6210ecd0c640fdb12e97990e1ed6bb2f9d6c6e3
Anton Gladyshev <returnstrike@yandex.ru>
2021-09-11 18:06:17 +0300

    draft for irregular regular

---
+++ irregular_regular_verbs.js
@@ -0,0 +1,324 @@
+topicName = "Irregular verbs";
+rules = {
+"1st":[
+{"qst":"I",
+ "conj": "1st",
+ "noun": "yo",
+ "ans":"-o"
+},
+{"qst":"you (informal)",
+ "conj": "1st",
+ "noun": "tú",
+ "ans":"-as"
+},
+{"qst":"he",
+ "conj": "1st",
+ "noun": "él",
+ "ans":"-a"
+},
+{"qst":"she",
+ "conj": "1st",
+ "noun": "ella",
+ "ans":"-a"
+},
+{"qst":"you (formal)",
+ "conj": "1st",
+ "noun": "usted",
+ "ans":"-a"
+},
+{"qst":"we (musculine)",
+ "conj": "1st",
+ "noun": "nosotros",
+ "ans":"-amos"
+},
+{"qst":"we (feminine)",
+ "conj": "1st",
+ "noun": "nosotras",
+ "ans":"-amos"
+},
+{"qst":"you (plural, musculine)",
+ "conj": "1st",
+ "noun": "vosotros",
+ "ans":"-áis"
+},
+{"qst":"you (plural, feminine)",
+ "conj": "1st",
+ "noun": "vosotras",
+ "ans":"-áis"
+},
+{"qst":"they (musculine)",
+ "conj": "1st",
+ "noun": "ellos",
+ "ans":"-an"
+},
+{"qst":"they (feminine)",
+ "conj": "1st",
+ "noun": "ellas",
+ "ans":"-an"
+},
+{"qst":"you (plural, formal)",
+ "conj": "1st",
+ "noun": "ustedes",
+ "ans":"-an"
+}],
+"2nd":[
+{"qst":"I",
+ "conj": "2nd",
+ "noun": "yo",
+ "ans":"-o"
+},
+{"qst":"you (informal)",
+ "conj": "2nd",
+ "noun": "tú",
+ "ans":"-es"
+},
+{"qst":"he",
+ "conj": "2nd",
+ "noun": "él",
+ "ans":"-e"
+},
+{"qst":"she",
+ "conj": "2nd",
+ "noun": "ella",
+ "ans":"-e"
+},
+{"qst":"you (formal)",
+ "conj": "2nd",
+ "noun": "usted",
+ "ans":"-e"
+},
+{"qst":"we (musculine)",
+ "conj": "2nd",
+ "noun": "nosotros",
+ "ans":"-emos"
+},
+{"qst":"we (feminine)",
+ "conj": "2nd",
+ "noun": "nosotras",
+ "ans":"-emos"
+},
+{"qst":"you (plural, musculine)",
+ "conj": "2nd",
+ "noun": "vosotros",
+ "ans":"-éis"
+},
+{"qst":"you (plural, feminine)",
+ "conj": "2nd",
+ "noun": "vosotras",
+ "ans":"-éis"
+},
+{"qst":"they (musculine)",
+ "conj": "2nd",
+ "noun": "ellos",
+ "ans":"-en"
+},
+{"qst":"they (feminine)",
+ "conj": "2nd",
+ "noun": "ellas",
+ "ans":"-en"
+},
+{"qst":"you (plural, formal)",
+ "conj": "2nd",
+ "noun": "ustedes",
+ "ans":"-en"
+}],
+"3rd":[
+{"qst":"I",
+ "conj": "3rd",
+ "noun": "yo",
+ "ans":"-o"
+},
+{"qst":"you (informal)",
+ "conj": "3rd",
+ "noun": "tú",
+ "ans":"-es"
+},
+{"qst":"he",
+ "conj": "3rd",
+ "noun": "él",
+ "ans":"-e"
+},
+{"qst":"she",
+ "conj": "3rd",
+ "noun": "ella",
+ "ans":"-e"
+},
+{"qst":"you (formal)",
+ "conj": "3rd",
+ "noun": "usted",
+ "ans":"-e"
+},
+{"qst":"we (musculine)",
+ "conj": "3rd",
+ "noun": "nosotros",
+ "ans":"-imos"
+},
+{"qst":"we (feminine)",
+ "conj": "3rd",
+ "noun": "nosotras",
+ "ans":"-imos"
+},
+{"qst":"you (plural, musculine)",
+ "conj": "3rd",
+ "noun": "vosotros",
+ "ans":"-ís"
+},
+{"qst":"you (plural, feminine)",
+ "conj": "3rd",
+ "noun": "vosotras",
+ "ans":"-ís"
+},
+{"qst":"they (musculine)",
+ "conj": "3rd",
+ "noun": "ellos",
+ "ans":"-en"
+},
+{"qst":"they (feminine)",
+ "conj": "3rd",
+ "noun": "ellas",
+ "ans":"-en"
+},
+{"qst":"you (plural, formal)",
+ "conj": "3rd",
+ "noun": "ustedes",
+ "ans":"-en"
+}]
+}
+root_rules = {
+"e-ie":[
+{"qst":"I",
+ "conj": "1st",
+ "noun": "yo",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"you (informal)",
+ "noun": "tú",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"he",
+ "noun": "él",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"she",
+ "noun": "ella",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"you (formal)",
+ "noun": "usted",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"we (musculine)",
+ "noun": "nosotros",
+ "inf": "e",
+ "ans": "e"
+},
+{"qst":"we (feminine)",
+ "noun": "nosotras",
+ "inf": "e",
+ "ans": "e"
+},
+{"qst":"you (plural, musculine)",
+ "noun": "vosotros",
+ "inf": "e",
+ "ans": "e"
+},
+{"qst":"you (plural, feminine)",
+ "noun": "vosotras",
+ "inf": "e",
+ "ans": "e"
+},
+{"qst":"they (musculine)",
+ "noun": "ellos",
+ "inf": "e",
+ "ans": "ie"},
+{"qst":"they (feminine)",
+ "noun": "ellas",
+ "inf": "e",
+ "ans": "ie"
+},
+{"qst":"you (plural, formal)",
+ "noun": "ustedes",
+ "inf": "e",
+ "ans": "ie"
+}],
+"o-ue":[
+{"qst":"I",
+ "conj": "1st",
+ "noun": "yo",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"you (informal)",
+ "noun": "tú",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"he",
+ "noun": "él",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"she",
+ "noun": "ella",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"you (formal)",
+ "noun": "usted",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"we (musculine)",
+ "noun": "nosotros",
+ "inf": "o",
+ "ans": "o"
+},
+{"qst":"we (feminine)",
+ "noun": "nosotras",
+ "inf": "o",
+ "ans": "o"
+},
+{"qst":"you (plural, musculine)",
+ "noun": "vosotros",
+ "inf": "o",
+ "ans": "o"
+},
+{"qst":"you (plural, feminine)",
+ "noun": "vosotras",
+ "inf": "o",
+ "ans": "o"
+},
+{"qst":"they (musculine)",
+ "noun": "ellos",
+ "inf": "o",
+ "ans": "ue"},
+{"qst":"they (feminine)",
+ "noun": "ellas",
+ "inf": "o",
+ "ans": "ue"
+},
+{"qst":"you (plural, formal)",
+ "noun": "ustedes",
+ "inf": "o",
+ "ans": "ue"
+}],
+}
+cards = [
+{"tgt":"querer",
+ "ntv":"to want",
+ "conj": "1st",
+ "root_rule": "e-ie",
+ "pos": 2,
+ "img":"to_want.png"},
+{"tgt":"poder",
+ "ntv":"to can",
+ "conj": "1st",
+ "root_rule": "e-ie",
+ "pos": 1,
+ "img":"can.png"}
+]
